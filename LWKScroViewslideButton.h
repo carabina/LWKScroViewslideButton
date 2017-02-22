857d7a9cbb3c94dde98e72ec5fad13471fdf4ab7
LwkMumu <17767200757@163.com>
2017-02-22 14:59:39 +0800

    第一次提交

---
+++ LWKScroViewslideButton.h
@@ -0,0 +1,22 @@
+//
+//  LWKScroViewslideButton.h
+//  Huihui
+//
+//  Created by 伟凯   刘 on 2017/2/21.
+//  Copyright © 2017年 pg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void(^selectIndex)(int,NSString *);
+
+@interface LWKScroViewslideButton : UIView <UIScrollViewDelegate>
+
+@property(nonatomic,assign)CGSize buttonSize;
+
+- (instancetype)initWithFrame:(CGRect)frame andButtonStrs:(NSArray *)strArray;
+- (void)setScrollViewBackColor:(UIColor *)ScrollViewBackColor andSetButtonTextColor:(UIColor *)ButtonTextColor andSetButtonSelectTextColor:(UIColor *)ButtonSelectTextColor;
+- (void)setButtonWidth:(CGFloat)width;
+- (void)setButtonTextFont:(CGFloat)font andIsBold:(BOOL)boolol;
+@property(nonatomic,copy)selectIndex selectBlack;
+@end
